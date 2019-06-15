59a441d3c96b578d0bdc917dcebac8e9baf53ebc
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 12:14:50 +0200

    Logical Operators

--- test.js
+++ test.js
@@ -1,40 +1,10 @@
-var x = 5
-x > 4 
-//true
-x >= 5
-//true
-x > 5
-//false
-x >= 4 
-//true
-x < 5
-//false
-x <= 5
-//true
-x == 4
-//false
-x == 5
-//true
-x === 5
-//true
-x = "5"
-//"5"
-x == 5
-//true
-x === 5
-//false
-x === "5"
-//true
-x = 5
-//5
-x != 4
-//true
-x != 5
-//false
-x !== 5
-//false
-x = "5"
-//"5"
-x !== 5
-//true
+var myAge = 22;
+
+if (myAge >= 18 && myAge <= 30){
+    
+    document.write("You can cone, you coll dude")
+} else{
+
+    document.write("you aint coming!")
+}
 
