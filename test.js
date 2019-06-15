8c389069f90ef6b44c4e3a4032533c024eeac0d0
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 13:43:30 +0200

    Arrays

--- test.js
+++ test.js
@@ -1,15 +1,27 @@
-var str = "hello world";
-str
-//hello, world"
-var str2 = str.slice(2, 9);
-str2
-//"llo, wo"
-var str3 = str.slice(2);
-str2
-//"llo, world"
-var tags = "meat, ham, salami, prok, beef, chicken"
-tags
-//"mear, ham salami, prok, beef, chicken"
-var tagsArrey = tags.split(",");
-tagsArrey
-// ["meat", "ham", "salami", "prok", "beef", "chicken"]
+var myArray = [];
+myArray[0] = 25
+//25
+myArray[0]
+//25
+myArray[1] = 35
+//35
+myArray[3] = "hello"
+//"hello"
+myArray
+//[25, 35, true, "hello"]
+console.log(myArray);
+//[25, 35, true, "hello"]
+myArray[2] = false//false
+myArray
+//[25, 35, false, "hello"]
+var myArray2 = [10, 20, "hi", false];
+myArray2
+//[10, 20, "hi", false]
+var myArray3 = new Array()
+var myArray4 = new Array(5)
+myArray2.length
+//4
+myArrey2.sort()
+//[10, 20, false, "hi"]
+myArray2.reverse()
+//["hi", false, 20, 10]
