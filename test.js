642f60c1e2fa771ae9dfdde85875f12c997b7afb
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 17:09:34 +0200

    Constructor Functions

--- test.js
+++ test.js
@@ -1,28 +1,30 @@
 
-
-var myCar2 = {maxSpeed: 0, driver: "Net Ninja", drive: function (speed, time): {
-    console.log(speed * time);
-    );
-    }
-    test: function(){
-        console.log(myCar2);
+/*
+var myCar = {
+    maxSpeed: 70, driver: "Michal", drive: function(speed, time){
+        console.log(speed * time);
     },
     logDriver: function(){
-        console.log("griver name is" + this.driver);
+        console.log("driver name is " + this.driver);
     }
+}*/
 
-/*
-    var myCar3 = {maxSpeed: 0, driver: "Michal", drive: function (speed, time): {
-        console.log(speed * time);
-        );
-        }
-        test: function(){
-            console.log(myCar3);
-        }
-*/
+var Car = function(maxSpeed, driver){
 
-myCar2.logDriver();
-myCar2.test();
-//myCar3.test();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+        this.maxSpeed = maxSpeed;
+        this.driver = driver;
+        this.drive = function(speed, time){
+            console.log)speed * time);
+        };
+        this.logDriver = function(){
+            console.log("driver name is " + this.driver);
+        };
+}
+
+var myCar = new Car (70, "Michal");
+var myCar2 = new Car (40, "Krzys");
+var myCar3 = new Car (10, "Jurek");
+var myCar4 = new Car (90, "Smurf");
+
+myCar.drive(30,5);
+myCar3.logDriver();
