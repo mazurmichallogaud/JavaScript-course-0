e4313a0a023583ba95e1cabfe8998c51233761bc
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 11:53:24 +0200

    If Statements

--- test.js
+++ test.js
@@ -1,37 +1,14 @@
-var iLikeMeat = true
-iLikeMeat //true
-iLikeMeat = "true" //"true"
-iLikeMeat = false //false
-7 > 5
-//true
-7 > 5
-//false
-7 = 7
-//uncaught ReferenceError
-7 ==7
-//true
-7 == 5
-//false
-0
-0
-Boolean(7 > 5)
-//true
-Boolean(6)
-//true
-Boolean(-5)
-//true
-Boolean(0)
-//fals
-Boolean("hello")
-//uncaught ReferenceError
-Boolean("hello")
-//true
-Boolean("")
-//false
+var youLikeMeat = true;
+var myNum = 10;
 
+if (youLikeMeat){
 
+    document.write("you like meat");
 
+} else{
 
+    document.write("you hate meat");
+}
 
 
 
