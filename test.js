2107f8c1e7513c385f7949a8ab0ef5fb6de596e3
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 11:25:21 +0200

    Logging to the Console

--- test.js
+++ test.js
@@ -1,22 +1,11 @@
-var myVar = 5
-myVar += 
+var myVar = "Michal"
+document.write(myVar)
+console.log(myVar) //Michal
+document.write(10)
+console.log(50)
+console.log(20 + 5) //100
+document.write(20 / 5)
 
-myVar + 3 //=8
-myVar = myVar + 10 //=15
-10 - 5 //=5
-myVar - 3 //+12, bo myVar jest już równy 15 (za linijką 5)
-myVar = myVar - 5 //=10
-myVar //10
-10 * 10 //100
-20 / 10 //2
-myVar = myVar / 2
-5
-myVar //5
-5 + 5 //10
-5 + "hello" // "5hello"
-100 + "hello" //"100hello"
-"hello" + "michal" //"hellomichal"
-"hello" + " michal" //"hello michal"
 
 
 
