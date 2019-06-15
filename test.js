c5c991342667beb9d4bedfdc78f6fd27a3ab2d3e
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 17:52:02 +0200

    Window onLoad Event

--- test.js
+++ test.js
@@ -1,15 +1,21 @@
-var content = document.getElementById("content");
-var button = document.getElementById("swoe-more");
 
-button.onclick = function(){
 
-    if(content.className == "open")
-        //shrink the box
-        content.className = "";
-        button/innetHTMS = "Show More";
-} else{
-    //expand the box
-    content.className = "open";
-    button.innterHTML = "Show Less";    
-}
+window.onload = function(){
+
+    var content = document.getElementById("content");
+    var button = document.getElementById("swoe-more");
+    
+    button.onclick = function(){
+    
+        if(content.className == "open")
+            //shrink the box
+            content.className = "";
+            button/innetHTMS = "Show More";
+    } else{
+        //expand the box
+        content.className = "open";
+        button.innterHTML = "Show Less";    
+    }
+};
+
 };
