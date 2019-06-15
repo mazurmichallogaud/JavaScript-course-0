3926ccd0ad75eaafed50f93a1d4059981287233c
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 15:55:49 +0200

    Creating a new JavaScript Object

--- test.js
+++ test.js
@@ -1,9 +1,20 @@
-var myCar = new Car()
-var myString = new String()
-myStrung = "hello";
-//"hello"
-myString.length
-//5
-myString.toLowerCase()
-//"hello"
-var myString2 = "hi there";
+var myArray = new Array();
+myArrey[0] = 8;
+myArray[1] = "hello";
+
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Michal";
+myCar.drive = function(){
+    console.log("now driving"););
+}
+
+myCar.drive();
+
+var myCar2 = {maxSpeed: 0, driver: "Net Ninja", drive: function (speed, time): {
+    console.log(speed * time);
+    );
+    }
+
+console.log(myCar2.maxSpeed);
+myCar2.drive();
