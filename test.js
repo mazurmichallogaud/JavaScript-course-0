25df985934cef081aefbd1f05dfa55705b15ee62
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 15:46:24 +0200

    Introduction to Objects

--- test.js
+++ test.js
@@ -1,27 +1,9 @@
-var myArray = [];
-myArray[0] = 25
-//25
-myArray[0]
-//25
-myArray[1] = 35
-//35
-myArray[3] = "hello"
+var myCar = new Car()
+var myString = new String()
+myStrung = "hello";
 //"hello"
-myArray
-//[25, 35, true, "hello"]
-console.log(myArray);
-//[25, 35, true, "hello"]
-myArray[2] = false//false
-myArray
-//[25, 35, false, "hello"]
-var myArray2 = [10, 20, "hi", false];
-myArray2
-//[10, 20, "hi", false]
-var myArray3 = new Array()
-var myArray4 = new Array(5)
-myArray2.length
-//4
-myArrey2.sort()
-//[10, 20, false, "hi"]
-myArray2.reverse()
-//["hi", false, 20, 10]
+myString.length
+//5
+myString.toLowerCase()
+//"hello"
+var myString2 = "hi there";
