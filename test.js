9eabcb31857ebb9da6ea1884dbe332dc6ae4dd6b
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 11:58:02 +0200

    If Statements

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
-var youLikeMeat = true;
-var myNum = 10;
+var myAge = 22
 
-if (youLikeMeat){
+if (myAge > 30){
 
-    document.write("you like meat");
+    document.write("you are over 30!");
+} else if (myAge > 20){
 
+    document.write("you are over 20!");
+} else if (myAge > 10 ){
+
+    document.write("you are over 10!");
 } else{
 
-    document.write("you hate meat");
+    document.write("you are not over 10!");
 }
-
 
 
