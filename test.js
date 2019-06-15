48c5c5da09ae5dd412ffc8f878e2a53f0be94997
Michał Mazur <mazur.michal.logaud@gmail.com>
2019-06-15 12:46:40 +0200

    Break&Continue

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
-var links = document.getElementsByTagName("a");
+for (i = 0; i < 10; i++) {
+
+        if(i === 5 && i === 3){
+
+            continue;
+        }
 
 
-for (i = 0; i <= links.length; i++) {
+    console.log(i);
 
-    console.log("this is link number" + i);
+    if(i === 7){
+
+        break;
+    }
 
 }
 
-document.write("all links now looped");
-
+console.log("I have broken out of the loop")
